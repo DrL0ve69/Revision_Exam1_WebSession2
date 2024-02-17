5e750c0e9246af00991a95ce2ba04379834e0962
phili <0758510@cegepoutaouais.qc.ca>
2024-02-17 16:39:54 -0500

    début 3?

---
+++ Formulaire_Journaliste/script.js
@@ -0,0 +1,4 @@
+// 1.Créer objets/classes Journalistes et Équipe de journalistes.
+// 2. Manipulation du DOM et ses variables
+// 3. Soumission du formulaire et vérification des champs *** PAS DE COULEUR IDENTIQUE
+// 4. Ajout du journaliste dans l'équipe (<span>)
