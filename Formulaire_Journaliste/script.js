cf16908da4450480f695c68b19cef456a1610e32
phili <0758510@cegepoutaouais.qc.ca>
2024-02-20 01:01:28 -0500

    Objets/Classe créés manque les contraintes et sauvegarde de l'équipe vers une autre page

--- Formulaire_Journaliste/script.js
+++ Formulaire_Journaliste/script.js
@@ -1,16 +1,6 @@
-// 1. Créer objets/classes Journalistes et Équipe de journalistes.
-
 
 /*[abc]	: Peut accepter n’importe quel caractère de la série / [0-9] : idem mais, du premier caractère au dernier
 (x|y)		: Peut accepter une ou l’autre des expressions / (x){5}		: Permet d’exiger un certain nombre d’occurrences (ex. 5)*/
-// 2. Manipulation du DOM, les variables et RegEx
-//  2.1 Enregistrer les entrées des <<input>> dans des variables ou utilisation de Jquery →
-//   2.1.1 Mettre les valeurs des <<inputs>> dans les variables :
-
-
-
-//   2.1.2 Identifier les balises et manipuler le <<innerText>> ou <<textContent>> à l'aide de variables ou Jquery
-
 
 //  2.3
 //   2.3.1 RegExNom → Contraintes :
@@ -20,12 +10,16 @@
 // 3. Soumission du formulaire et vérification des champs *** PAS DE COULEUR IDENTIQUE
 
 function VerifierFormulaire()
-{
+{// 1. Manipulation du DOM, les variables et RegEx, créer objets/classes Journalistes et Équipe de journalistes.
 	let inputNom = document.querySelector('#nomTexte').value;
 	let inputBiographie = document.querySelector('#biographie').value;
 	let inputSpecialite = document.querySelector('#inputGroupSelect01').value;
 	let inputCouleur = document.querySelector('#couleurPreferee').value;
 
+
+	let journaliste = {nom:inputNom, biographie:inputBiographie, specialite:inputSpecialite, couleur:inputCouleur};
+	let equipeJournaliste = {};
+	equipeJournaliste += journaliste;
 
 	if (/[A-Z][a-z]/.test(inputNom) !== true || inputNom === null || inputBiographie === null)
 	{
@@ -33,8 +27,9 @@ function VerifierFormulaire()
 	}
 	else
 	{
-		document.querySelector('#membresEquipe').innerText += "Nom : " + inputNom + "\n" + "Specialité : " + inputSpecialite + "\n" + "Couleur favorite : " + inputCouleur + "\n\n" ;
-		console.log(inputSpecialite,inputBiographie,inputNom,inputCouleur);
+		document.querySelector('#membresEquipe').innerText += "Nom : " + inputNom + "\n" + "Specialité" +
+			" : " + inputSpecialite + "\n" + "Couleur favorite : " + inputCouleur + "\n\n" + journaliste.nom;
+		console.log(inputSpecialite,inputBiographie,inputNom,inputCouleur,equipeJournaliste);
 	}
 }
 
