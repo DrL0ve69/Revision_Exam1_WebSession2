e1cd91cd657d90fdd900332c1060651bcb85e3e9
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2024-04-02 10:47:10 -0400

    waywyawyawya trop de trucs à faire voir fetch ajax api et mockAPI

--- ProjetFinal_PhilippeCharron/script.js
+++ ProjetFinal_PhilippeCharron/script.js
@@ -14,3 +14,72 @@ function CreerNouveauMembre(){
         $('#email').val(), $('#nomUtilisateur').val(), $('#passwordCreation').val()) ;
     // Ajouter le membre au JSON et à la page des membres.
 }
+
+
+fetch('https://mockapi.io/clone/660c04c03a0766e85dbd2c18/users', {
+    method: 'GET',
+    mode: 'cors' ,
+    credentials: 'include' ,
+    headers: {'content-type':'application/json'},
+}).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+    // handle error
+}).then(tasks => {
+    // Do something with the list of tasks
+}).catch(error => {
+    // handle error
+})
+
+const newTask = {
+    content: 'Check out mockapi.io',
+    completed: false,
+};
+
+fetch('https://mockapi.io/clone/660c04c03a0766e85dbd2c18/users', {
+    method: 'POST',
+    headers: {'content-type':'application/json'},
+    // Send your data in the request body as JSON
+    body: JSON.stringify(CreerNouveauMembre())
+}).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+    // handle error
+}).then(task => {
+    // do something with the new task
+}).catch(error => {
+    // handle error
+})
+
+fetch('https://mockapi.io/clone/660c04c03a0766e85dbd2c18/'+ Membre.id, {
+    method: 'PUT', // or PATCH
+    headers: {'content-type':'application/json'},
+    body: JSON.stringify({completed: true})
+}).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+    // handle error
+}).then(task => {
+    // Do something with updated task
+}).catch(error => {
+    // handle error
+})
+function Supprimer(id){
+
+    fetch('https://mockapi.io/clone/660c04c03a0766e85dbd2c18/users/'+Membre.id, {
+        method: 'DELETE',
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        // handle error
+    }).then(task => {
+        // Do something with deleted task
+    }).catch(error => {
+        // handle error
+    })
+}
+
