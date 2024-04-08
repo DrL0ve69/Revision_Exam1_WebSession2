c52c976307e81091b2867d8628e1d0740e747fd5
phili <0758510@cegepoutaouais.qc.ca>
2024-04-08 03:03:57 -0400

    Html : manque le BootStrap... Sinon reste les restrictions des inputs ainsi que le JQuery pour l'ajout et retrait des notes.

--- ProjetFinal_PhilippeCharron/script.js
+++ ProjetFinal_PhilippeCharron/script.js
@@ -1,16 +1,23 @@
 // 1. Définir la classe Membre et ses propriétés :
+let dataJson = document.href;
+function Membre(nom, prenom, email, nomUtilisateur, password) {
 
-function Membre(nom, prenom, telephone, codePostal, email, nomUtilisateur, password) {
-
-    this.Nom = nom; this.Prenom = prenom; this.Telephone = telephone; this.CodePostal = codePostal; this.Email = email;
+    this.Nom = nom; this.Prenom = prenom;  this.Email = email;
     this.NomUtilisateur = nomUtilisateur; this.Password = password;
     // Ajouter après l'ajout du nouveau membre → this.Pointage = pointage;
-    this.Id = id++;                                   // à tester sinon public static Id => private id=0; id+=;
+    let id = 0;
+    this.Id += id; // à tester sinon public static Id => private id=0; id+=;
+    
 }
 
-function CreerNouveauMembre(){
+function CreerNouveauMembre( key, value ){
 
-    let nouveauMembre = new Membre($('#nom').val(), $('#prenom').val(), $('#numTelephone').val(), $('#codePostal').val(),
+    let nouveauMembre = new Membre($('#nom').val(), $('#prenom').val(),
         $('#email').val(), $('#nomUtilisateur').val(), $('#passwordCreation').val()) ;
+    function toString()
+    {
+        return `${this.Id},. ${this.Nom}, ${this.Prenom}`;
+    }
+    dataJson += localStorage.setItem( "Membres", `${nouveauMembre.Id},. ${nouveauMembre.Nom}, ${nouveauMembre.Prenom}` );
     // Ajouter le membre au JSON et à la page des membres.
 }
